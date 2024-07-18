59d3325af4796f1944750071a13b2383c4a3bc3b
LuterGS <koo04034@gmail.com>
2024-07-18 20:11:35 +0900

    [FIX] CORS allow all 로 설정 2

--- main.py
+++ main.py
@@ -36,12 +36,13 @@ app = FastAPI()
 
 origins = [
     "https://kaba.team",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "http://localhost"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
